f419e025bfedbcd8001fd24fddd2f45c2fe64c9c
bellagrin10 <55347549+bellagrin10@users.noreply.github.com>
2021-08-09 20:26:38 +0300

    Update task_8_3.py

--- HomeWork8/task_8_3.py
+++ HomeWork8/task_8_3.py
@@ -24,12 +24,11 @@ from functools import wraps
 def type_logger(func):
     @wraps(func)
     def wrapper(*args, **kwargs):
-        args_type = {arg: type(arg) for arg in args}
-        kwargs_type = {kwargs[kwarg]: type(kwargs[kwarg]) for kwarg in kwargs}
-        func_result_type = {func(*args, **kwargs): type(func(*args, **kwargs))}
-        result_types = {**args_type, **kwargs_type, **func_result_type}
-        result = f'{func.__name__}{result_types}'.replace('{', '(').replace('}', ')')
-        return result
+        args_type = [f'{arg}: {type(arg)}' for arg in args]
+        kwargs_type = [f'{kwargs[kwarg]}: {type(kwargs[kwarg])}' for kwarg in kwargs]
+        func_result_type = f'{func(*args, **kwargs)}: {type(func(*args, **kwargs))}'
+        result_types = [*args_type, *kwargs_type, func_result_type]
+        return f"{func.__name__}({', '.join(result_types)})"
     return wrapper
 
 
